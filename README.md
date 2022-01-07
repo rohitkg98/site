14fd42b82bfcafbdc97350a0f77103ee4e708620
Kaushal Rohit <rohit.kg98@gmail.com>
2022-01-07 20:34:32 +0530

    init next.js site

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # site
+
+The source code for my personal [site](https://kaushal-rohit.dev). Built using Next.js. Powered by Vercel.
---
+++ pages/index.tsx
@@ -0,0 +1,61 @@
+import { format, parseISO } from 'date-fns';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import React from 'react';
+import Layout from '../components/Layout';
+import { getAllPosts } from '../lib/api';
+import { PostType } from '../types/post';
+
+type IndexProps = {
+  posts: PostType[];
+};
+
+export const Index = ({ posts }: IndexProps): JSX.Element => {
+  return (
+    <Layout>
+      <h1>Kaushal Rohit&apos;s Site</h1>
+      <p>Hi there 👋</p>
+      <p>
+        I&apos;m Kaushal Rohit, a Software Developer, and this is my Personal
+        Website. I&apos;m currently working as a Platform Engineer at Benzinga,
+        where I build high-throughput and low-latency financial services in
+        Golang.
+      </p>
+      <p>
+        When I&apos;m not working, I usually spend my time building things,
+        reading books, playing games or watching movies.
+      </p>
+
+      {posts.map((post) => (
+        <article key={post.slug} className="mt-12">
+          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+            {format(parseISO(post.date), 'MMMM dd, yyyy')}
+          </p>
+          <h1 className="mb-2 text-xl">
+            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+              <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
+                {post.title}
+              </a>
+            </Link>
+          </h1>
+          <p className="mb-3">{post.description}</p>
+          <p>
+            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+              <a>Read More</a>
+            </Link>
+          </p>
+        </article>
+      ))}
+    </Layout>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts(['date', 'description', 'slug', 'title']);
+
+  return {
+    props: { posts },
+  };
+};
+
+export default Index;
